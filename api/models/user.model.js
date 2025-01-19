e316661a99639cbe88398de91ca8ddcf996549ee
firsa <firsaavln@gmail.com>
2025-01-19 20:14:58 +0700

    create signup api route and middleware

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import { mongoose } from "mongoose";
 
-const userSchema = new MongoServerError.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
